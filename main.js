18e645d64b24518e46e0eaa8a733d9b7b4ff756c
allana123456 <115155133+allana123456@users.noreply.github.com>
2024-05-16 17:30:06 -0300

    nnnn

--- main.js
+++ main.js
@@ -20,9 +20,8 @@ const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4  = new Date("2024-02-01T00:00:00");
 
-
-
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1]. textContent =calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
